741cfac5cd662130fb8266670e3e552b30177e5d
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-16 13:53:39 +0530

    LC-asteroid-collision [Runtime: N/A; Memory: N/A]

---
+++ stack/asteroid-collision.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        vector<int< st;
+        for(int a : asteroid){
+            
+            
+            while(s.size() > 0 && s.back() > 0 && a < 0  && s.back() < abs(a)){
+                s.pop_back();
+
+
+                if(){
+
+                }
+
+                else if (){
+                    st.push_back(a);
+                }
+                else {
+                    
+                }
+            }
+
+        }
+        
+        
+    }
+};
