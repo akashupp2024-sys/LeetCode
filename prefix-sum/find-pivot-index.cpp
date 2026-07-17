b763434142c7b48472719b97dcfca5a8dcaa52b9
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-17 17:14:59 +0530

    LC-find-pivot-index [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/find-pivot-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftsum = 0;
+        int totalsum = 0;
+
+        for(int num : nums){
+            totalsum = totalsum + num;
+
+
+        }
+
+        for(int i = 0; i< nums.size(); i++){
+            int rightsum = totalsum - leftsum - nums[i];
+
+            if(rightsum == leftsum){
+                return i;
+            }
+
+            leftsum = leftsum + nums[i]
+        }
+
+        return -1;
+
+        
+    }
+};
