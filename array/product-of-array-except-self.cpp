63df2f41c468f51f6bc025f12e182f8995974710
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-18 22:16:23 +0530

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

--- array/product-of-array-except-self.cpp
+++ array/product-of-array-except-self.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+
+        vector<int>ans;
+
+       for(int i = 0; i< nums.size(); i++){
+        int leftpro = 1;
+        int rightpro = 1;
+
+        for(int j = 0; j< i; j++){
+            leftpro = leftpro * nums[j];
+        }
+
+        for(int j = i + 1; j< nums.size(); j++){
+
+            rightpro = rightpro * nums[j];
+        }
+
+        ans.push_back(leftpro * rightpro);
+
+       }
+
+       return ans;
+
         
     }
 };
