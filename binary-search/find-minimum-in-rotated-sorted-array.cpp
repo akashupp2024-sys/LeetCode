c051c65f0d56dce74fd8f4cb913d0137437764d7
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-19 00:24:02 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-minimum-in-rotated-sorted-array.cpp
+++ binary-search/find-minimum-in-rotated-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int findMin(vector<int>& nums) {
         int minval = nums[0];
 
-        for(int i = ; i<nums.size(); i++){
+        for(int i = 1; i<nums.size(); i++){
             minval = min(minval, nums[i]);
         }
         
