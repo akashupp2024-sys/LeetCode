28216b0bb9d02f3ecd2eb83d0397165e264a10ac
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-19 16:16:21 +0530

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int currsum = 0;
+        int maxsum = 0;
+
+        for(int i = 0; i< nums.size(); i++){
+            currsum = surrsum + nums[i];
+            maxsum = max(currsum, maxsum);
+
+            if(currsum < 0){
+                currsum = 0;
+            }
+        }
+        return maxsum;
+        
+    }
+};
