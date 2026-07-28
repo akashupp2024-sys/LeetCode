1b07a11b11204178154d46b9299aed8478781f2f
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-28 21:55:53 +0530

    LC-rearrange-array-elements-by-sign [Runtime: N/A; Memory: N/A]

---
+++ array/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        vector<int> arr1;
+        vector<int> arr2;
+
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] > 0){
+                arr1.push_back(nums[i]);
+            }
+            else {
+                arr2.push_back(nums[i]);
+            }
+        }
+        vector<int> ans;
+        int j= 0, k = 0;
+        while(j < arr1.size() && k < arr2.size()){
+            ans.push_back(arr1[j]);
+            ans.psuh_back(arr2[k]);
+            j++;
+            k++;
+        }
+
+        return ans;
+
+        
+    }
+};
