865c30325dcc9de03a556b71da1ce92eef55df2f
akashupp2024-sys <akashupp2024@gmail.com>
2026-07-31 21:12:16 +0530

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- array/binary-search.cpp
+++ array/binary-search.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i = 0; i< n; i++){
-            if(nums[i] == target){
-                return i;
+    //     int n = nums.size();
+    //     for(int i = 0; i< n; i++){
+    //         if(nums[i] == target){
+    //             return i;
+
+    //         }
+
+    //     }
+
+    //     return -1;
+
+        int st = nums[0];
+        int end = nums.size()-1;
+        while (st <= end){
+            int mid = st + (end - st)/2;
+
+           
+            if (nums[mid] > target){
+                end  = mid - 1;
 
             }
+            else if (nums[mid] < target){
 
+                st = mid + 1;
+            }
+            else {
+                return mid;
+            }
+           
+
+            
         }
 
         return -1;
+
+        
+
         
     }
 };
