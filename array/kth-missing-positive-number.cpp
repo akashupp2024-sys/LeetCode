5e4a6acd4e8042e08260ce03ff18f49d23fe9779
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-02 18:59:23 +0530

    LC-kth-missing-positive-number [Runtime: N/A; Memory: N/A]

---
+++ array/kth-missing-positive-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        
+        for(int num : arr){
+            if(num < k){
+                k++;
+            }
+            // else {
+            //     break;
+            // }
+        }
+
+        return k;
+    }
+};
