f869fecfb9020b6a0b049916e42efbec3d7d67a4
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-03 23:47:17 +0530

    LC-time-needed-to-buy-tickets [Runtime: N/A; Memory: N/A]

--- simulation/time-needed-to-buy-tickets.cpp
+++ simulation/time-needed-to-buy-tickets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int time = 0;
-        int target = tickets[i];
+        int target = tickets[k];
 
         for(int i = 0; i< ticket.size(); i++){
             if(i <= k){
