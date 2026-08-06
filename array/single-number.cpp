ca4e986e3cb8a58ecb45d683cdc64ffeba1ae83f
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-06 18:36:41 +0530

    LC-single-number [Runtime: N/A; Memory: N/A]

--- array/single-number.cpp
+++ array/single-number.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
 
-        sort(nums.begin(), nums.end());
-
+        int ans = nums[0];
         for(int i = 0; i< nums.size(); i++){
-            int j = i + 1;
-
-            if((nums[i] ^ nums[j]) == 1){
-                return nums[i];
-            }
+            ans = ans ^ nums[i];
         }
 
-        retur
+        return ans;
         
     }
 };
