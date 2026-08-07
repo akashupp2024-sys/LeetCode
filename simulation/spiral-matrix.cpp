bbf8014465c19ce7ee15b9f725dcab08d5ee514c
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-08 00:13:41 +0530

    LC-spiral-matrix [Runtime: N/A; Memory: N/A]

---
+++ simulation/spiral-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        int top = 0;
+        int buttom = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+
+        vector<int> ans;
+
+        while(top<= buttom && right <= left){
+            for(int j = left; j <= right; j++){
+                ans.push_back(matrix[top][j]);
+            }
+            top++;
+
+            for(int i = top; i<= buttom; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            while(top <= buttom){
+                for(int j = right; i>= left; j--){
+                    ans.push_back(matrix[buttom][j]);
+                }
+                buttom--;
+            }
+
+            while(right <= left){
+                for(int i = button; i>= top; i--){
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        
+    }
+};
