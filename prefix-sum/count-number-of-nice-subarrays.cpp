84aec8b16a6b24abe039ebb456aa7b51b59b622d
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-15 19:47:36 +0530

    LC-count-number-of-nice-subarrays [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+
+        int s = 0;
+        int curr = 0, count = 0, zeros = 0;
+
+        for (int e = 0; e < nums.size(); e++) {
+            if (nums[e] % 2 == 0) {
+                nums[e] = 0;
+
+            } else {
+                nums[e] = 1;
+            }
+        }
+
+        for (int e = 0; e < nums.size(); e++) {
+            curr = curr + nums[e];
+
+            while (k < curr) {
+                curr = curr - nums[s];
+                s++;
+            }
+
+            if (curr == k) {
+
+                while (s < e && nums[s] == 0) {
+                    zeros++;
+                    s++;
+                }
+
+                count += zeros + 1;
+            }
+        }
+
+        return count;
+    }
+};
