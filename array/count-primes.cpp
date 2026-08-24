ee96fd2d56a14e38f9c1ed9ea5a353b2edf93c13
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-24 14:22:38 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- array/count-primes.cpp
+++ array/count-primes.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n <= 2) return 0;
 
-        vector<bool> prime(n,true);
-        prime[0] = prime[1] = false;
-        for(int i = 2; i * i < n; i++){
-            if( prime[i]){
-                for(int j = i * i; j <n; j+=i){
-                    prime[j] = false;
+        
+        if (n <= 2) return 0;
 
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
-        int count = 0;
-        for(int i = 2; i< n; i++){
-            if( prime[i]){
-                count++;
-            }
+
+        int ans = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i])
+                ans++;
         }
 
-        return count;
+        return ans;
         
     }
 };
