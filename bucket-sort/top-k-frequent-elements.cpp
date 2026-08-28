e8572c6728f9bee7adb790afcbd1d42810ab6f4f
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-28 20:18:32 +0530

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

---
+++ bucket-sort/top-k-frequent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int>freq;
+
+        for(int x : nums){
+            freq[x]++;
+        }
+
+        priority_queue<pair<int, int>>pq;
+
+        for(auto x : freq){
+
+            pq.push({x.second, x.first});
+        }
+
+        vector<int> ans;
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+        
+    }
+};
