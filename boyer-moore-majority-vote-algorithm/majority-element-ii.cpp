262dcd045374da1caad13a2811ce6b35c9affd33
akashupp2024-sys <akashupp2024@gmail.com>
2026-08-31 14:20:07 +0530

    LC-majority-element-ii [Runtime: N/A; Memory: N/A]

--- boyer-moore-majority-vote-algorithm/majority-element-ii.cpp
+++ boyer-moore-majority-vote-algorithm/majority-element-ii.cpp
@@ -10,10 +10,10 @@ public:
                 count1++;
             } else if (x == candidate2) {
                 count2++;
-            } else if (candidate1 == 0) {
+            } else if (count1 == 0) {
                 candidate1 = x;
                 count1 = 1;
-            } else if (candidate2 == 0) {
+            } else if (count2 == 0) {
                 candidate2 = x;
                 count2 = 1;
             } else {
