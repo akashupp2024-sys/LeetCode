ae54f95f022d9ac82d3c4cf4f3e74c904e523fd8
akashupp2024-sys <akashupp2024@gmail.com>
2026-09-08 16:54:20 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

---
+++ boyer-moore-majority-vote-algorithm/majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int st = 0;
+        int end = nums.size();
+        int mid = st + (end-st)/2;
+        return nums[mid];
+
+        
+    }
+};
