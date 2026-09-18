51580207fa2faf0c830177e76441da759f84dd88
akashupp2024-sys <akashupp2024@gmail.com>
2026-09-18 09:40:17 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.cpp
+++ math/climbing-stairs.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n <= 2)
-        return n;
-        int a = 1;
-        int b = 2;
-        for(int i = 3; i <= n; i++)
-        {
-           int c = a + b;
-            a = b;
-            b = c;
 
+    int climbrec(int n){
+        if(n <= 2){
+            return n;
         }
-        return b;
+
+        return climbrec(n-1) + climbrec(n-2);
+
+    }
+    int climbStairs(int n) {
+        // if(n <= 2)
+        // return n;
+        // int a = 1;
+        // int b = 2;
+        // for(int i = 3; i <= n; i++)
+        // {
+        //    int c = a + b;
+        //     a = b;
+        //     b = c;
+
+        // }
+        // return b;
+
+        return climbrec(n); 
         
     }
+
 };
