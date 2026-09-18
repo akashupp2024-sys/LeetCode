ef1e3189ab56d442efcd46534d863dd1b07ee1b2
akashupp2024-sys <akashupp2024@gmail.com>
2026-09-18 10:21:59 +0530

    LC-min-cost-climbing-stairs [Runtime: N/A; Memory: N/A]

--- array/min-cost-climbing-stairs.cpp
+++ array/min-cost-climbing-stairs.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int climbmemo(vector<int>& cost, int  i, vector<int>& dp){
+        if(i >= cost.size()){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        dp[i] =  cost[i] + min(climbmemo(cost,i+1, dp), climbmemo(cost, i+2, dp));
+
+
+        return dp[i];
+
+    }
 
     int climbrec(vector<int>& cost, int i){
         if(i >= cost.size()){
@@ -8,9 +22,13 @@ public:
         return cost[i] + min(climbrec(cost,i+1), climbrec(cost, i+2));
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int i;
+        
 
-        return min(climbrec(cost, 0), climbrec(cost, 1));
+        // return min(climbrec(cost, 0), climbrec(cost, 1));
+
+        vector<int>dp(cost.size(), -1);
+
+        return min(climbmemo(cost , 0, dp), climbmemo(cost, 1, dp));
         
     }
 };
